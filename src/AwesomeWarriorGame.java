0788fd6a581c93c04f753c93d017b03efa795c41
AntonioF01 <antonio.bbr@gmail.com>
2022-05-21 17:12:53 +0100

    Initial commit

---
+++ src/AwesomeWarriorGame.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class AwesomeWarriorGame {
+
+    private final String PAYS = "Pays";
+
+
+    private final int challenges;
+    private final int decisions;
+
+    private int initialChallenge;
+    private int finalChallenge;
+    private int initialEnergy;
+
+    private final List<Edge>[] graph;
+
+    @SuppressWarnings("unchecked")
+    public AwesomeWarriorGame(int challenges, int decisions) {
+        this.graph = new LinkedList[challenges];
+        this.challenges = challenges;
+        this.decisions = decisions;
+    }
+
+    public void handleConnection(int finishedChallenge, String action, int energy, int newChallenge) {
+        graph[finishedChallenge].add(new Edge(newChallenge, action.equals(PAYS) ? -energy : energy));
+    }
+
+    public void processFinalLine(int finalChallenge, int initialChallenge, int initialEnergy){
+        this.finalChallenge = finalChallenge;
+        this.initialChallenge = initialChallenge;
+        this.initialEnergy = initialEnergy;
+    }
+
+    private static class Edge {
+        private final int node;
+        private final int weight;
+
+        public Edge(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+
+        /*@Override
+        public boolean equals(Object other) {
+            if (other == this) return true;
+            if (!(other instanceof Edge)) return false;
+            return ((Edge) other).node == this.node;
+        }*/
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader((System.in)));
+
+        String[] aux = in.readLine().split(" ");
+        int challenges = Integer.parseInt(aux[0]);
+        int decisions = Integer.parseInt(aux[1]);
+
+        AwesomeWarriorGame game = new AwesomeWarriorGame(challenges, decisions);
+
+        for (int i = 0; i < decisions; i++) {
+            aux = in.readLine().split("");
+            game.handleConnection(Integer.parseInt(aux[0]), aux[1], Integer.parseInt(aux[2]), Integer.parseInt(aux[3]));
+        }
+
+        aux = in.readLine().split("");
+        game.processFinalLine(Integer.parseInt(aux[0]), Integer.parseInt(aux[1]), Integer.parseInt(aux[2]));
+    }
+
+
+}
